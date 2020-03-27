81fdaefce5f08eed51fc9e8d4a89ca98fe844bea
voidblaze <ftv.voidblaze@gmail.com>
2020-03-27 02:54:41 +0200

    Add more info

--- server.js
+++ server.js
@@ -54,7 +54,15 @@ async function readIfNew(page) {
 
 let runFunc = async () => {
   const browser = await ppt.launch({
-    args: ["--no-sandbox"]
+    args: [
+      "--no-sandbox",
+      "--disable-setuid-sandbox",
+      "--disable-infobars",
+      "--window-position=0,0",
+      "--ignore-certifcate-errors",
+      "--ignore-certifcate-errors-spki-list",
+      '--user-agent="Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/65.0.3312.0 Safari/537.36"'
+    ]
   });
   const page = await browser.newPage();
   await page.goto(url);
