9242a6e2c7b1db2ede93b773244acbf0528bea32
voidblaze <ftv.voidblaze@gmail.com>
2020-03-27 02:08:24 +0200

    Refactor on v2

--- server.js
+++ server.js
@@ -1,98 +1,75 @@
-const env = require('dotenv').config()
-const req = require('request');
-const notifier = require('node-notifier')
-const moment = require('moment');
-const opn = require('opn');
+const env = require("dotenv").config();
+const req = require("request");
+const notifier = require("node-notifier");
+const moment = require("moment");
+const opn = require("opn");
+const cheerio = require("cheerio");
+const sg = require("@sendgrid/mail");
+const ppt = require("puppeteer-extra"); // Add stealth plugin and use defaults (all tricks to hide puppeteer usage)
+const StealthPlugin = require("puppeteer-extra-plugin-stealth");
+ppt.use(StealthPlugin());
+
+// Add adblocker plugin to block all ads and trackers (saves bandwidth)
+const AdblockerPlugin = require("puppeteer-extra-plugin-adblocker");
+ppt.use(AdblockerPlugin({ blockTrackers: true }));
+sg.setApiKey(process.env.SENDGRID);
 
 let url = process.env.XE_SEARCH_URL;
-let loginurl = "https://my.xe.gr/app/login";
 
-notifier.on('click', function (notifierObj, options) {
-    opn(url);
-})
+notifier.on("click", function(notifierObj, options) {
+  opn(url);
+});
 
-let previousResultCount = 0;
-
-let urlLoginInfo = {
-    email: process.env.XE_USER,
-    password: process.env.XE_PASSWORD,
-    autologin: 1,
-    redirect: "https://www.xe.gr/"
-}
-
-let reqLoginHeaders = {
-    Accept: "*/*",
-    "Cache-Control": "no-cache",
-    Connection: "keep-alive",
-    "Content-Type": "application/x-www-form-urlencoded",
-    Host: "my.xe.gr",
-    Origin: "https://my.xe.gr",
-    Referer: "https://my.xe.gr/login?redirect=https://www.xe.gr/",
-    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/74.0.3729.169 Safari/537.36",
-    "X-Requested-With": "XMLHttpRequest"
-};
-let reqSearchHeaders = {
-    Accept: "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3",
-    "Cache-Control": "no-cache",
-    Connection: "keep-alive",
-    Host: "www.xe.gr",
-    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/74.0.3729.169 Safari/537.36",
-    "X-Requested-With": "XMLHttpRequest"
-}
+let previousResultCount = 2;
 
 function l(message) {
-    console.log(moment().format("MMMM Do YYYY, HH:mm:ss") + " | " + message)
+  console.log(moment().format("MMMM Do YYYY, HH:mm:ss") + " | " + message);
 }
 
-async function doLogin() {
-    req.post({
-        url: loginurl,
-        jar: true,
-        form: urlLoginInfo,
-        headers: reqLoginHeaders
-    }, function (err, httpResponse, body) {
-        if (err) throw err;
-        return true;
-    })
+async function readIfNew(page) {
+  await page.reload();
+  let foundText = "";
+  try {
+    foundText = await page.$eval(".count_classifieds", el => el.textContent);
+  } catch (ex) {
+    console.error(ex);
+  }
+
+  let found = Number(foundText.split(" ")[1].split("")[0]);
+
+  if (found != 0) {
+    l(`Βρέθηκαν ${found} νέες αγγελίες!`);
+
+    if (previousResultCount < found) {
+      sg.send({
+        to: process.env.OBSERVERS.split(","),
+        from: "ftv.voidblaze@gmail.com",
+        subject: "Βρέθηκαν Νέες Αγγελίες στην XE!",
+        html: 'Δες εδώ: <a href="' + url + '">Χρυσή Ευκαιρία</a>'
+      });
+
+      previousResultCount = found;
+    } else {
+      l("Όσες και πριν");
+    }
+  } else {
+    l("Δεν βρέθηκαν αποτελέσματα");
+  }
 }
 
-async function readIfNew() {
-
-    l("Doing login")
-    await doLogin();
-    l("Completed login")
-
-    req.get({
-        url: url,
-        jar: true,
-        headers: reqSearchHeaders
-    }, function (err, httpResponse, body) {
-        l("Got Results")
-        let found = parseInt(body.match(/Βρέθηκαν <strong>([0-9]*)<\/strong> αγγελίες με τα κριτήρια που έχετε επιλέξει/)[1]);
-        if (found != 0) {
-            l(`Βρέθηκαν ${found} νέες αγγελίες!`)
-
-            if (previousResultCount < found) {
-                notifier.notify({
-                    title: 'Νέα Αγγελία!',
-                    message: 'Βρέθηκε μια νέα αγγελία!',
-                    sound: true,
-                    wait: true
-                })
-                previousResultCount = found;
-            } else {
-                l('Όσες και πριν')
-            }
-        } else {
-            l("Δεν βρέθηκαν αποτελέσματα")
-        }
-    })
-}
+let runFunc = async () => {
+  const browser = await ppt.launch({ headless: false });
+  const page = await browser.newPage();
+  await page.goto(url);
+  await readIfNew(page);
+  //await browser.close();
+};
 
 (async () => {
-    l("Starting Process")
-    await doLogin();
-    setInterval(async () => {
-        await readIfNew();
-    }, process.env.SEARCH_INTERVAL || 10000)
+  l("Starting Process");
+  l(process.env.XE_SEARCH_URL);
+
+  await runFunc();
+
+  setInterval(runFunc, process.env.SEARCH_INTERVAL || 10000);
 })();
